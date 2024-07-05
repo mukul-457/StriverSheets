8bf69a3722de2b490e84b774db5b4d85406c13ff
mukul-457 <mukulacharya.1911@gmail.com>
2024-07-06 02:15:27 +0530

    Easy. kadence to cal max sub array sum

---
+++ SDE/MaxSubArray.java
@@ -0,0 +1,17 @@
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] inp = new int[]{1,2};
+    }
+    
+}
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int LargestSum = nums[0];
+        int curr_sum = 0;
+        for (int index = 0 ; index < nums.length ; index ++){
+            curr_sum = Math.max(nums[index] + curr_sum , nums[index]);
+            LargestSum = Math.max(curr_sum , LargestSum);
+        }
+        return LargestSum ;
+    }
+}
