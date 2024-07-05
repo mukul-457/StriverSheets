4feecf3e86a128d9115e77b299079a2c15bc9326
mukul-457 <mukulacharya.1911@gmail.com>
2024-07-06 02:24:24 +0530

    Easy. solved it in O of N

---
+++ SDE/BuyAndSellStock.java
@@ -0,0 +1,14 @@
+public class BuyAndSellStock {
+    
+}
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxp = 0;
+        int buy  = prices[0];
+        for(int index = 1; index < prices.length; index++){
+            buy = Math.min(buy, prices[index]);
+            maxp = Math.max(maxp , prices[index]- buy);
+        }
+        return maxp;
+    }
+}
