e2d21774df18c1a297a31cacd22fe6a802ec946a
mukul-457 <mukulacharya.1911@gmail.com>
2024-08-23 14:57:05 +0530

    heap Kth larget |  quick select was another option wich was faster .try that

---
+++ SDE/KthLargestElement.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class KthLargestElement {
+    public static void main(String[] args) {
+        System.out.println(KL.findKthLargest(new int[]{3}, 1));
+    }
+}
+
+
+class KL {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int ele : nums){
+            pq.add((Integer) ele);
+        }
+        k = nums.length-k+1;
+        int ans=0;
+        while(k >0){
+            ans = (int) pq.poll();
+            k--;
+        }
+        return ans;
+    }
+}
